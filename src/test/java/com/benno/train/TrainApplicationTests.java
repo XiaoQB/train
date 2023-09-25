3aae68645c652d4e55ce59dc60d670d01f54feb4
bennoxiao <plus2019@foxmail.com>
2023-09-25 18:51:59 +0800

    init

---
+++ src/test/java/com/benno/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benno.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
