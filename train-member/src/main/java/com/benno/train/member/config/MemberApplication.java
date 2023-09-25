a81522c4607a60f9bd6c60d0206cf1a16e24d502
bennoxiao <plus2019@foxmail.com>
2023-09-25 19:56:21 +0800

    add logger

--- train-member/src/main/java/com/benno/train/member/config/MemberApplication.java
+++ train-member/src/main/java/com/benno/train/member/config/MemberApplication.java
@@ -1,16 +1,24 @@
 package com.benno.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
 @ComponentScan("com.benno")
 public class MemberApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
+		SpringApplication app = new SpringApplication(MemberApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("start success!");
+		LOG.info("test address: \thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
 	}
 
 }
