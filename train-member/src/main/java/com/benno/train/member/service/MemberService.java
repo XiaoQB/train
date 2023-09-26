02f2b3b4ab0630b4f24d0919a49037604fe142a3
bennoxiao <plus2019@foxmail.com>
2023-09-26 11:56:26 +0800

    register func

--- train-member/src/main/java/com/benno/train/member/service/MemberService.java
+++ train-member/src/main/java/com/benno/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.benno.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.benno.train.member.domain.Member;
+import com.benno.train.member.domain.MemberExample;
 import com.benno.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -13,4 +18,21 @@ public class MemberService {
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
     }
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isNotEmpty(members)) {
+            throw new RuntimeException("the number is already registered!");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
+    }
+
+
 }
