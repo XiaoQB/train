dc0dac33b852c75b214500f9e0e1c77585d1431e
bennoxiao <plus2019@foxmail.com>
2023-09-26 14:15:16 +0800

    snow algorithm for id

--- train-member/src/main/java/com/benno/train/member/service/MemberService.java
+++ train-member/src/main/java/com/benno/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.benno.train.member.service;
 import cn.hutool.core.collection.CollUtil;
 import com.benno.train.common.exception.BusinessException;
 import com.benno.train.common.exception.BusinessExceptionEnum;
+import com.benno.train.common.util.SnowUtil;
 import com.benno.train.member.domain.Member;
 import com.benno.train.member.domain.MemberExample;
 import com.benno.train.member.mapper.MemberMapper;
@@ -32,7 +33,7 @@ public class MemberService {
         }
 
         Member member = new Member();
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
